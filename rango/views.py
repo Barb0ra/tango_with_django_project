8dfd5362c3843f15ea1fe09dcb5dce3570a35568
Barbora Barancikova <2439071b@student.gla.ac.uk>
2021-01-29 16:11:50 +0100

    6.3

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from rango.models import Category
+from django.http import HttpResponse
 
 # Create your views here.
-from django.http import HttpResponse
+
 def index(request):
     #construct a dictionary to pass to the template engine as its context.
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
